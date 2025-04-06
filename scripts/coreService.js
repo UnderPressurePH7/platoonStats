558079d9fe093d926e4fe9494903307a951c8e63
Vasyl <149047913+UnderPressurePH7@users.noreply.github.com>
2025-04-06 11:24:39 +0300

    Update coreService.js

--- scripts/coreService.js
+++ scripts/coreService.js
@@ -299,7 +299,7 @@ class CoreService {
     const playersID = this.getPlayersIds();
     this.curentPlayerId = this.sdk.data.player.id.value;
 
-    if (playersID.length >= 1 && !this.isInPlatoon) return;
+    // if (playersID.length >= 1 && !this.isInPlatoon) return;
 
     this.PlayersInfo[this.curentPlayerId] = this.sdk.data.player.name.value;
 
