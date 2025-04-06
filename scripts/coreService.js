a0a48b5c1db96aa679ccd310fb078de94df726a3
UnderPressurePH7 <149047913+UnderPressurePH7@users.noreply.github.com>
2025-04-06 12:17:13 +0300

    null

--- scripts/coreService.js
+++ scripts/coreService.js
@@ -21,7 +21,7 @@ class CoreService {
       const state = JSON.parse(savedState);
       this.BattleStats = state.BattleStats || {};
       this.PlayersInfo = state.PlayersInfo || {};
-      this.curentPlayerId = state.curentPlayerId || this.sdk.data.player.id.value;
+      this.curentPlayerId = state.curentPlayerId || null;
       this.curentArenaId = state.curentArenaId || null;
       this.curentVehicle = state.curentVehicle || null;
       this.isInPlatoon = state.isInPlatoon || false;
